4177a80845e3df910b794498c200854aa63cc729
NearHuscarl <near.huscarl@gmail.com>
2018-08-30 17:59:29 +0700

    create layout for landscape and xlarge devices

--- app/src/main/java/com/nearhuscarl/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/nearhuscarl/swoosh/Controller/LeagueActivity.kt
@@ -4,23 +4,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.nearhuscarl.swoosh.Utilities.EXTRA_LEAGUE
+import com.nearhuscarl.swoosh.Model.Player
 import com.nearhuscarl.swoosh.R
+import com.nearhuscarl.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
     var selectedLeague = ""
+    var player = Player("", "")
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
+
     fun onMenClicked(view: View) {
         womenLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "men"
+        player.league = "men"
     }
 
 
@@ -28,20 +42,20 @@ class LeagueActivity : BaseActivity() {
         menLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "women"
+        player.league = "women"
     }
 
     fun onCoedClicked(view: View) {
         menLeagueBtn.isChecked = false
         womenLeagueBtn.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
     }
 
     fun leagueNextClicked(view: View) {
-        if (selectedLeague != "") {
+        if (player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         } else {
             Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
